4de868463c56cc575bdbecd7b1c8946dc02c068f
Ngoc Long Phung NGUYEN <K170040@Student.Kent.edu.au>
2018-05-16 15:32:54 +1000

    Updated a bunch of stuffs

--- javascript/bookings.js
+++ javascript/bookings.js
@@ -1,4 +1,12 @@
-//Add the adult and children suffix to input field
-let adult = document.getElementById("adult");
-let children = document.getElementById("children");
+let book = document.getElementById("book");
+let serviceHeader = document.getElementById("service-header");
+let firstfield = document.getElementById("firstfield");
+let destination = document.getElementById("destination");
+let calendar = document.getElementById("calendar");
+let people = document.getElementById("people");
+let lastfield = document.getElementById("lastfield");
 
+function revealTour() {
+	book.style.display = "initial";
+	serviceHeader.innerHTML = "Tour Packages";
+}
